4eda5fc3327c26fdd56b80790c4b5cfb21b37c54
camille raiser <camille.raiser@hotmail.fr>
2018-08-09 18:21:51 +0200

    index, show, new cocktail feature and add dose feature

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,26 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(params_cocktail)
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def params_cocktail
+    params.require(:cocktail).permit(:name)
+  end
+end
