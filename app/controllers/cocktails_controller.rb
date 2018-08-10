e7690a71de56dbd8c7af187e00a997856e30f11d
camille raiser <camille.raiser@hotmail.fr>
2018-08-10 16:32:26 +0200

    added photos and styling

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -22,6 +22,6 @@ class CocktailsController < ApplicationController
   private
 
   def params_cocktail
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
